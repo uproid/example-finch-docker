fde58d1bbb200d479720aab8ea95ec92b177df46
uproid <info@uproid.com>
2026-05-17 14:22:18 +0000

    Update from main repo

---
+++ lib/app.dart
@@ -0,0 +1,173 @@
+import 'languages/language_dart.g.dart';
+import 'widgets/widget_dart.g.dart';
+import 'package:finch/model.dart';
+import 'core/local_events.dart';
+import 'package:finch/finch_route.dart';
+import 'db/job_collection_free.dart';
+import 'db/person_collection_free.dart';
+import 'package:finch/finch_model_less.dart';
+import 'db/example_collections.dart';
+import 'models/example_model.dart';
+import 'package:finch/finch_console.dart';
+import 'package:finch/finch_app.dart';
+import 'package:finch/finch_tools.dart';
+import 'route/socket_route.dart';
+import 'route/web_route.dart';
+import 'package:finch/finch_capp.dart';
+
+FinchConfigs configs = FinchConfigs(
+  pathCache: pathTo(env['PATH_CACHE'] ?? './cache_routes'),
+  jinjaMapTemplate: mapTemplates,
+  widgetsPath: pathTo(env['WIDGETS_PATH'] ?? "./lib/widgets"),
+  widgetsType: env['WIDGETS_TYPE'] ?? 'j2.html',
+  languagePath: pathTo(env['LANGUAGE_PATH'] ?? "./lib/languages"),
+  languageSource: LanguageSource.dart,
+  dartLanguages: languageDart,
+  publicDir: pathTo(env['PUBLIC_DIR'] ?? './public'),
+  dbConfig: FinchDBConfig(
+    enable: true, //env['ENABLE_DATABASE'] == 'true',
+    dbName: env['MONGODB_NAME'] ?? 'example',
+    auth: env['MONGODB_AUTH'] ?? 'admin',
+    pass: env['MONGODB_PASSWORD'] ?? 'PasswordMongoDB',
+    host: env['MONGODB_CONNECTION'] ?? 'localhost',
+    port: env['MONGODB_PORT'] ?? '27018',
+    user: env['MONGODB_USER'] ?? 'root',
+  ),
+  port: (env['DOMAIN_PORT'] ?? '8085').toInt(def: 8085),
+  mysqlConfig: FinchMysqlConfig(
+    enable: true,
+    host: env['MYSQL_HOST'] ?? 'localhost',
+    port: (env['MYSQL_PORT'] ?? '3306').toInt(def: 3306),
+    user: env['MYSQL_USER'] ?? 'example_user',
+    pass: env['MYSQL_PASSWORD'] ?? 'example_password',
+    databaseName: env['MYSQL_DATABASE'] ?? 'example_db',
+  ),
+
+  /// Enable local debugger
+  enableLocalDebugger: (env['ENABLE_LOCAL_DEBUGGER'] ?? true).toString().toBool,
+
+  /// SQLite configuration
+  sqliteConfig: FinchSqliteConfig(
+    enable: true,
+    filePath: env['SQLITE_PATH'] ?? './example_database.sqlite',
+  ),
+);
+
+final app = FinchApp(configs: configs);
+var jobCollectionFree = JobCollectionFree(db: app.mongoDb);
+var personCollectionFree = PersonCollectionFree(db: app.mongoDb);
+
+final socketManager = SocketManager(
+  app,
+  event: SocketEvent(
+    onConnect: (socket) {
+      app.socketManager?.sendToAll(
+        "New user connected! count: ${app.socketManager?.countClients}",
+        path: "output",
+      );
+      socket.send(
+        {'message': 'Soccuess connect to socket!'},
+        path: 'connected',
+      );
+    },
+    onMessage: (socket, data) {},
+    onDisconnect: (socket) {
+      var count = app.socketManager?.countClients ?? 0;
+      app.socketManager?.sendToAll(
+        "User disconnected! count: ${count - 1}",
+        path: "output",
+      );
+    },
+  ),
+  routes: getSocketRoute(),
+);
+
+void main([List<String>? args]) async {
+  /// Example Web Route
+  app.addRouting(getWebRoute);
+
+  /// Add custom commands
+  app.commands.add(
+    CappController('example', options: [
+      CappOption(
+        name: 'test',
+        shortName: 't',
+        description: 'An example option',
+      ),
+    ], run: (c) async {
+      if (c.existsOption('test')) {
+        CappConsole.writeTable(
+          [
+            ['Column 1', 'Column 2', 'Column 3'],
+            ...List.filled(5, ['Data 1', 'Data 2', 'Data 3'])
+          ],
+          dubleBorder: true,
+          color: CappColors.warning,
+        );
+      }
+
+      return CappConsole(
+        'This is an example command from Finch App! Time: ${DateTime.now()}',
+        CappColors.success,
+      );
+    }),
+  );
+
+  /// Or add routes directly one by one
+  app
+    ..get(
+      path: '/get',
+      index: (rq) async {
+        return rq.renderString(text: 'Hello from ${rq.method} /get request!');
+      },
+    )
+    ..postGet(
+      path: '/post',
+      index: (rq) async {
+        return rq.renderString(text: 'Hello from ${rq.method} /post request!');
+      },
+    );
+
+  Request.localEvents.addAll(localEvents);
+  Request.addLocalLayoutFilters(localLayoutFilters);
+  app.start(args).then((value) {
+    Console.p("Example app started: http://localhost:${value.port}");
+  });
+
+  /// Example Cron job
+  app.registerCron(
+    /// Evry 2 days clean the example collection of database
+    FinchCron(
+      schedule: FinchCron.evryDay(2),
+      onCron: (index, cron) async {
+        if (app.mongoDb.isConnected) {
+          ExampleCollections().deleteAll();
+        }
+      },
+      delayFirstMoment: true,
+    ).start(),
+  );
+
+  app.registerCron(
+    /// Add evry hour a new document to the example collection of database
+    FinchCron(
+      schedule: "0 * * * *",
+      onCron: (index, cron) async {
+        if (app.mongoDb.isConnected) {
+          ExampleCollections().insertExample(ExampleModel(
+            title: DateTime.now().toString(),
+            slug: 'slug-$index',
+          ));
+        }
+      },
+      delayFirstMoment: true,
+    ).start(),
+  );
+
+  // Clear all route cache (memory and file) every restart
+  RouteCache.clearAllCache();
+}
+
+void printDB() {
+  DBCollectionFree.printDesign();
+}
